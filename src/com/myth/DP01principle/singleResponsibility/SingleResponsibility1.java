141b709356aab48e526413068df31310c25157e6
VmythV <1635170329@qq.com>
2021-02-04 14:40:58 +0800

    add 01principle

---
+++ src/com/myth/DP01principle/singleResponsibility/SingleResponsibility1.java
@@ -0,0 +1,25 @@
+package com.myth.DP01principle.singleResponsibility;
+
+/**
+ * @author myth
+ * @Date 2021-02-04 14:32:30
+ */
+public class SingleResponsibility1 {
+
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle();
+        vehicle.run("汽车");
+        vehicle.run("飞机");
+    }
+
+}
+
+// 交通工具类
+// 方 式 1
+// 1. 在方式 1 的 run 方法中，违反了单一职责原则
+// 2. 解决的方案非常的简单，根据交通工具运行方法不同，分解成不同类即可
+class Vehicle {
+    public void run(String vehicle) {
+        System.out.println(vehicle + " 在公路上运行....");
+    }
+}
