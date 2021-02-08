3157e8dbe13ff9faed635c804167210760611243
VmythV <1635170329@qq.com>
2021-02-08 23:29:47 +0800

    format Singleton type3

--- DP01Singleton/src/type3/SingletonTest03.java
+++ DP01Singleton/src/type3/SingletonTest03.java
@@ -33,12 +33,10 @@ class Singleton {
     //提供一个静态的公有方法，当使用到该方法时，才去创建 instance
     //即懒汉式
     public static Singleton getInstance() {
-        if(instance == null) {
-        instance = new Singleton();
-    }
+        if (instance == null) {
+            instance = new Singleton();
+        }
         return instance;
     }
-
-
 
 }
