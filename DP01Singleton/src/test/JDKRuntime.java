6e152655c9478bb92bb9606f4a7ad15f7b349c37
VmythV <1635170329@qq.com>
2021-02-11 21:32:55 +0800

    add Runtime

---
+++ DP01Singleton/src/test/JDKRuntime.java
@@ -0,0 +1,23 @@
+package test;
+
+/**
+ * @author myth
+ * @Date 2021-02-11 21:28:39
+ * java.lang.Runtime 经典的单例模式(饿汉式)
+ */
+public class JDKRuntime {
+    public static void main(String[] args) {
+        Runtime runtime = Runtime.getRuntime();
+        MyRuntime myRuntime = MyRuntime.getMyRuntime();
+    }
+}
+
+class MyRuntime{
+    private static MyRuntime MycurrentRuntime = new MyRuntime();
+
+    public static MyRuntime getMyRuntime() {
+        return MycurrentRuntime;
+    }
+
+    private MyRuntime() {}
+}
