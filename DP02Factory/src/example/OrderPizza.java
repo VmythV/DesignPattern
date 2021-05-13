31802133175bade18a50b9135cb40702d579dcc2
meiweiding <meiweiding@jsservice.cn>
2021-05-13 13:22:23 +0800

    add Factory example

---
+++ DP02Factory/src/example/OrderPizza.java
@@ -0,0 +1,38 @@
+package example;
+
+/**
+ * @author meiweiding
+ * @Date 2021-05-13
+ */
+public class OrderPizza {
+    /*
+    // 构造器
+    public OrderPizza() {
+        Pizza pizza = null;
+        String orderType; //  订购披萨的类型
+        do {
+            orderType = getType();
+            if (orderType.equals("greek")) {
+                pizza = new GreekPizza();
+                pizza.setName(" 希腊披萨 ");
+            } else if (orderType.equals("cheese")) {
+                pizza = new CheesePizza();
+                pizza.setName(" 奶酪披萨 ");
+            } else if (orderType.equals("pepper")) {
+                pizza = new PepperPizza();
+                pizza.setName("胡椒披萨");
+            } else {
+                break;
+            }
+            //输出 pizza 制作过程
+            pizza.prepare();
+            pizza.bake();
+            pizza.cut();
+            pizza.box();
+
+        } while (true);
+
+    }
+
+     */
+}
